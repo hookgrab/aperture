08a1769d1d7a407a2218b29537c69e7cc9a3e944
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-04-06 08:11:30 -0400

    Update import path

--- src/app/proto/layout.tsx
+++ src/app/proto/layout.tsx
@@ -4,8 +4,8 @@ export const metadata: Metadata = {
   title: "HookGrab",
   description: "Test you webhooks in Real-Time",
 	other: {
-		"go-import": "hg.atrin.dev/proto mod https://github.com/hookgrab/hg-proto",
-		// "go-import": "hg.atrin.dev/proto mod https://git.atrin.dev/hookgrab/hg-proto",
+		"go-import": "hg.atrin.dev/proto git https://github.com/hookgrab/hg-proto",
+		// "go-import": "hg.atrin.dev/proto git https://git.atrin.dev/hookgrab/hg-proto",
 	}
 };
 
