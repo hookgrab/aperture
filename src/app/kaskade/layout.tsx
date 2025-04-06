621245570e652e715d29e83f3f316846f0441823
Atrin Hojjat <atrin.hojjat@gmail.com>
2025-04-06 15:24:04 -0400

    Add backend package

--- src/app/kaskade/layout.tsx
+++ src/app/kaskade/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "HookGrab",
-  description: "Test you webhooks in Real-Time | Proto Definitions",
+  description: "Test you webhooks in Real-Time | GO Backend",
 	other: {
-		"go-import": "hg.atrin.dev/proto git https://github.com/hookgrab/hg-proto",
-		// "go-import": "hg.atrin.dev/proto git https://git.atrin.dev/hookgrab/hg-proto",
+		"go-import": "hg.atrin.dev/kaskade git https://github.com/hookgrab/kaskade",
+		// "go-import": "hg.atrin.dev/kaskade git https://git.atrin.dev/hookgrab/kaskade",
 	}
 };
 
---
+++ src/app/kaskade/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function ProtoLib() {
+	return (
+		<div>
+			HookGrab Backend: 
+			<Link href="https://github.com/hookgrab/kaskade">Github</Link>{' '}
+			<Link href="https://git.atrin.dev/hookgrab/kaskade">Gitlab</Link>
+		</div>
+	)
+}
